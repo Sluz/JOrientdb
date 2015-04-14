46241fd58f6714a8b09011d9b6846c48f928f6e1
Cyril Bourgès <cyril@tapastreet.com>
2015-04-14 10:59:18 +0100

    java basic type

---
+++ lib/jorientdb/java.rb
@@ -0,0 +1,18 @@
+require 'rjb'
+
+module JOrientDB 
+  module Java
+    module Lang
+      Boolean            = Rjb::import('java.lang.Boolean')
+      String             = Rjb::import('java.lang.String')
+      Number             = Rjb::import('java.lang.Number')
+      Double             = Rjb::import('java.lang.Double')
+      Float              = Rjb::import('java.lang.Float')
+    end
+    
+    module Util
+      AbstractMap        = Rjb::import('java.util.AbstractMap')
+      AbstractCollection = Rjb::import('java.util.AbstractCollection')
+    end
+  end
+end
